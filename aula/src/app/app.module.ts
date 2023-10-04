90c9d7e90f65a99f8b48c6a21071f47ab7d3fd2d
Hasanessaad <hasancayedessaad@gmail.com>
2023-10-03 23:20:34 -1000

    ok

---
+++ aula/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonListComponent } from './person-list/person-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
